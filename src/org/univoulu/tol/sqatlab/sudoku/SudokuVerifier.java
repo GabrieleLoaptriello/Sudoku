43e70c41791a2fb6ce1205b7f291aa0a06b46a27
GabrieleLoaptriello <Gabriele@LAPTOP-5DP341KS>
2016-11-27 18:48:02 +0100

    added some tests

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -5,23 +5,34 @@ public class SudokuVerifier {
 	public int verify(String candidateSolution) {
 		int result = 0;
 		char[] candidateSolutionArray = candidateSolution.toCharArray();
-		
-		for(int i = 0; i < 81; i++) {
-			if(isInvalidDigit(candidateSolutionArray[i])) {
+
+		for (int i = 0; i < 81; i++) {
+			if (isInvalidDigit(candidateSolutionArray[i])) {
 				result = -1;
 			}
 		}
 		
+		
+		
+		if(candidateSolutionArray[0] == candidateSolutionArray[1]) {
+			result = -3;
+		} else if(candidateSolutionArray[0] == candidateSolutionArray[2]) {
+			result = -3;
+		} else if(candidateSolutionArray[0] == candidateSolutionArray[3]) {
+			result = -3;
+		}
+
+		return result;
+	}
+
+	private boolean isInvalidDigit(char candidateSolution) {
+		boolean result = false;
+
+		if (candidateSolution < '1' || candidateSolution > '9') {
+			result = true;
+		}
+
 		return result;
 	}
 	
-	private boolean isInvalidDigit(char candidateSolution) {
-		boolean result = false;
-		
-		if(candidateSolution < '1' || candidateSolution > '9') {
-			result = true;
-		}
-		
-		return result;
-	}
 }
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -8,35 +8,57 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 
 	private SudokuVerifier sudokuVerifier;
-	
+
 	@Before
 	public void setUp() {
-		
+
 		this.sudokuVerifier = new SudokuVerifier();
 	}
-	
+
 	@Test
-	public void firstCellContainsDigitsBetween1and9() {
-		
-		assertEquals(sudokuVerifier.verify("017369825632158947958724316825437169791586432346912758289643571573291684164875293"), -1);
+	public void firstCellDoesNotContainsDigitsBetween1and9() {
+
+		assertEquals(sudokuVerifier
+				.verify("017369825632158947958724316825437169791586432346912758289643571573291684164875293"), -1);
+	}
+
+	@Test
+	public void secondDoesNotCellContainsDigitsBetween1and9() {
+
+		assertEquals(sudokuVerifier
+				.verify("407369825632158947958724316825437169791586432346912758289643571573291684164875293"), -1);
+	}
+
+	@Test
+	public void thirdCellDoesNotContainsDigitsBetween1and9() {
+
+		assertEquals(sudokuVerifier
+				.verify("410369825632158947958724316825437169791586432346912758289643571573291684164875293"), -1);
+	}
+
+	@Test
+	public void CellsDoNotRespectFirstRule() {
+
+		assertEquals(sudokuVerifier
+				.verify("417369825632158947958724316820437169791586432346912758289643571573291684164875293"), -1);
+	}
+
+	@Test
+	public void firstElementInFirstRowIsEqualSecond() {
+
+		assertEquals(sudokuVerifier
+				.verify("447369825632158947958724316825437169791586432346912758289643571573291684164875293"), -3);
 	}
 	
 	@Test
-	public void secondCellContainsDigitsBetween1and9() {
-		
-		assertEquals(sudokuVerifier.verify("407369825632158947958724316825437169791586432346912758289643571573291684164875293"), -1);
+	public void firstElementInFirstRowIsEqualThird() {
+		assertEquals(sudokuVerifier
+				.verify("414369825632158947958724316825437169791586432346912758289643571573291684164875293"), -3);
 	}
 	
 	@Test
-	public void thirdCellContainsDigitsBetween1and9() {
-		
-		assertEquals(sudokuVerifier.verify("410369825632158947958724316825437169791586432346912758289643571573291684164875293"), -1);
+	public void firstElementInFirstRowIsEqualFourth() {
+		assertEquals(sudokuVerifier
+				.verify("417469825632158947958724316825437169791586432346912758289643571573291684164875293"), -3);
 	}
-	
-	@Test
-	public void CellsRespectFirstRule() {
-		
-		assertEquals(sudokuVerifier.verify("417369825632158947958724316820437169791586432346912758289643571573291684164875293"), -1);
-	}
- 
 }
